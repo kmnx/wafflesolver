eb93be461bcfd33963ace7261b9fb54608901bd2
knm4x <max.hampel@gmail.com>
2024-10-27 22:09:53 +0100

    could still be improved by using a map instead of iterating over strings

--- cycle_decomposition.py
+++ cycle_decomposition.py
@@ -2,6 +2,14 @@ import copy
 import heapq
 import time
 
+def solution_mapping(scrambled, solution,solved_at_start):
+    mapping = {}
+    for char in set(scrambled):  # Use set to avoid duplicate characters
+        positions = [i for i, s_char in enumerate(solution) if s_char == char and i not in solved_at_start]
+        mapping[char] = positions
+    return mapping
+
+
 
 def main(scrambled, solution):
     start_time = time.time()
@@ -9,42 +17,52 @@ def main(scrambled, solution):
     solutionstack = []
     bigstack = []
     solved_at_start = []
+    unsolved_tiles = 0
+    # weed out already solved positions
+    for i in range(len(scrambled)):
+        if scrambled[i] == solution[i]:
+            solved_at_start.append(i)
+
+    for i in range(len(scrambled)):
+        if scrambled[i] != solution[i]:
+            unsolved_tiles += 1
+    mapping = solution_mapping(scrambled, solution, solved_at_start)
+    
 
     if len(solution) == 49:
         success = 20
     elif len(solution) == 25:
         success = 10
-    # weed out already solved ones
-    for i in range(len(scrambled)):
-        if scrambled[i] == solution[i]:
-            solved_at_start.append(i)
+    ideal_cycles_number = unsolved_tiles - success
+    
     # generate starting swaps
     for i in range(len(scrambled)):
         if i not in solved_at_start:
-            for index, char in enumerate(solution):
-                if char == scrambled[i]:
+            #for index, char in enumerate(solution):
+            #    if char == scrambled[i]:
+            for index in mapping[scrambled[i]]:
                     localcycle = [i, index]
                     newwholecycle = [localcycle]
                     priority = 0
                     for cycle in newwholecycle:
                         if len(cycle) == 2:
                             priority += 2 * 10000
+                        
 
                     heapq.heappush(bigstack, (-priority, newwholecycle))
 
     while bigstack:
         _, wholecycle = heapq.heappop(bigstack)
         visitedlist = copy.deepcopy(solved_at_start)
+        #print(wholecycle)
 
         for cycle in wholecycle:
             for i in cycle:
                 visitedlist.append(i)
         if len(visitedlist) == len(solution):
+            
             solutionstack.append(wholecycle)
-            solvecounter = 0
-            for cycle in wholecycle:
-                solvecounter = solvecounter + len(cycle) - 1
-            if solvecounter == success:
+            if len(wholecycle) == ideal_cycles_number:
                 break
 
             continue
@@ -89,6 +107,10 @@ def main(scrambled, solution):
                                             cyclesumprio += 100
                                             if len(cycle) == 2:
                                                 priority += 2 * 10000
+                                            elif len(cycle) == 3:
+                                                priority += 1000
+                                            elif len(cycle) == 4:
+                                                priority += 100
 
                                         heapq.heappush(
                                             bigstack, (-priority, newwholecycle)
@@ -96,7 +118,6 @@ def main(scrambled, solution):
                     else:
                         if index not in visitedlist:
                             newwholecycle = copy.deepcopy(wholecycle)
-                            newlocalcycle = copy.deepcopy(localcycle)
                             newwholecycle[-1].append(index)
                             priority = 0
                             cyclesumprio = 0
@@ -104,6 +125,10 @@ def main(scrambled, solution):
                                 cyclesumprio += 100
                                 if len(cycle) == 2:
                                     priority += 2 * 10000
+                                elif len(cycle) == 3:
+                                    priority += 1000
+                                elif len(cycle) == 4:
+                                    priority += 100
 
                             heapq.heappush(bigstack, (-priority, newwholecycle))
 
@@ -143,9 +168,11 @@ def main(scrambled, solution):
 # solution = "curver o nafootz s eeater"
 # scrambled = "thfeccun h t isistasni n i husitisgl g o ndigceiu"
 # solution = "deficiti i h osustainc h l iunitings n c hsuggest"
-scrambled = "adotpwet a b rahscocde n e dtpibueen r h rtrgceuh"
-solution = "chopperh u r eabscondt t d htributee n c aragweed"
+#scrambled = "adotpwet a b rahscocde n e dtpibueen r h rtrgceuh"
+#solution = "chopperh u r eabscondt t d htributee n c aragweed"
 # scrambled = "tcvcsrou o r dbpneares o i itsueiett e g coiehkar"
 # solution = "revisito e e rbandageo t s etouristi r c ocheckup"
+scrambled = "henreubq n i tmerluree e q aduuotado o d yieearnc"
+solution = "nunneryo u q imarqueea t a ldoubtedi r o echeered"
 if __name__ == "__main__":
     main(scrambled, solution)
